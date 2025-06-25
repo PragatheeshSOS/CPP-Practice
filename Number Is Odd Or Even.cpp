5504b49602c8bfd4b4d467d359e5d438275af64c
Pragatheesh S <128950352+PragatheeshSOS@users.noreply.github.com>
2025-06-25 16:09:40 +0530

    Commit

--- Number Is Odd Or Even.cpp
+++ Number Is Odd Or Even.cpp
@@ -1,4 +1,5 @@
 //Number Is Odd Or Even.............................................
+//APPROACH 1......................................
 #include<iostream>
 using namespace std;
 
@@ -7,6 +8,32 @@ int main(){
   cout<<"Enter Number: ";
   cin>>number;
   (number%2)?cout<<"Odd":cout<<"Even";
+}
+
+//APPROACH 2......................................
+#include<iostream>
+int main(){
+  int number;
+  std::cout<<"Enter Number: ";
+  std::cin>>number;
+  if (number%2)
+    std::cout<<"Odd";
+  else
+    std::cout<<"Even";
+}
+
+//APPROACH 3......................................
+#include<iostream>
+using namespace std;
+
+bool isEven(int number){
+  return (!(number&1));
+}
+int main(){
+  int number;
+  cout<<"Enter Number: ";
+  cin>>number;
+  isEven(number)?cout<<"Even":cout<<"Odd";
 }
 /*
 INPUT:
