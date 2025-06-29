7272c01eb43e3297e2b43a96dce7fc7b2e1ed6d3
Pragatheesh S <128950352+PragatheeshSOS@users.noreply.github.com>
2025-06-29 13:56:14 +0530

    Commit

---
+++ Size Of Each Datatype.cpp
@@ -0,0 +1,24 @@
+// Size Of Each Datatype......................................................
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Size Of char: "<<sizeof(char)<<endl;
+    cout<<"Size Of int: "<<sizeof(int)<<endl;
+    cout<<"Size Of short int: "<<sizeof(short int)<<endl;
+    cout<<"Size Of long int: "<<sizeof(long int)<<endl;
+    cout<<"Size Of float: "<<sizeof(float)<<endl;
+    cout<<"Size Of double: "<<sizeof(double)<<endl;
+    cout<<"Size Of wchar_t: "<<sizeof(wchar_t)<<endl;
+    cout<<"Size Of long long int: "<<sizeof(long long int)<<endl;
+}
+/*
+OUTPUT:
+Size Of char: 1
+Size Of int: 4
+Size Of short int: 2
+Size Of long int: 8
+Size Of float: 4
+Size Of double: 8
+Size Of wchar_t: 4
+Size Of long long int: 8
+*/
