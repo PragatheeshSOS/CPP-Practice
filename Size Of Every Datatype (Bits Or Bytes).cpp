4ed28f41ec54e09bf4f268437d98d30782e2d6fe
Pragatheesh S <128950352+PragatheeshSOS@users.noreply.github.com>
2025-06-29 08:07:17 +0530

    Commit

---
+++ Size Of Every Datatype (Bits Or Bytes).cpp
@@ -0,0 +1,39 @@
+// Size Of Every Datatype (Bits Or Bytes)......................................................
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"CHAR_BIT: "<<CHAR_BIT<<endl;
+    cout<<"CHAR_MAX: "<<CHAR_MAX<<endl;
+    cout<<"CHAR_MIN: "<<CHAR_MIN<<endl;
+    cout<<"INT_MAX: "<<INT_MAX<<endl;
+    cout<<"INT_MIN: "<<INT_MIN<<endl;
+    cout<<"LONG_MAX: "<<(long)LONG_MAX<<endl;
+    cout<<"LONG_MIN: "<<(long)LONG_MIN<<endl;
+    cout<<"SCHAR_MAX: "<<SCHAR_MAX<<endl;
+    cout<<"SCHAR_MIN: "<<SCHAR_MIN<<endl;
+    cout<<"SHRT_MAX: "<<SHRT_MAX<<endl;
+    cout<<"SHRT_MIN: "<<SHRT_MIN<<endl;
+    cout<<"UCHAR_MAX: "<<UCHAR_MAX<<endl;
+    cout<<"UINT_MAX: "<<(unsigned int)UINT_MAX<<endl;
+    cout<<"ULONG_MAX: "<<(unsigned long)ULONG_MAX<<endl;
+    cout<<"USHRT_MAX: "<<(unsigned short)USHRT_MAX<<endl;
+    return 0;
+}
+/*
+OUTPUT Varies According To Compliers........................................
+CHAR_BIT: 8
+CHAR_MAX: 127
+CHAR_MIN: -128
+INT_MAX: 2147483647
+INT_MIN: -2147483648
+LONG_MAX: 9223372036854775807
+LONG_MIN: -9223372036854775808
+SCHAR_MAX: 127
+SCHAR_MIN: -128
+SHRT_MAX: 32767
+SHRT_MIN: -32768
+UCHAR_MAX: 255
+UINT_MAX: 4294967295
+ULONG_MAX: 18446744073709551615
+USHRT_MAX: 65535
+*/
