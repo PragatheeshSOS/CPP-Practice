937e18d49f063ccd1d27466a79493fb6d8aef2b0
Pragatheesh S <128950352+PragatheeshSOS@users.noreply.github.com>
2025-06-29 14:24:03 +0530

    Commit

--- Limit Of Every Datatype (Bits Or Bytes).cpp
+++ Limit Of Every Datatype (Bits Or Bytes).cpp
@@ -17,6 +17,25 @@ int main(){
     cout<<"UINT_MAX: "<<(unsigned int)UINT_MAX<<endl;
     cout<<"ULONG_MAX: "<<(unsigned long)ULONG_MAX<<endl;
     cout<<"USHRT_MAX: "<<(unsigned short)USHRT_MAX<<endl;
+
+    cout<<"\nFLT_MAX: "<<(float)FLT_MAX<<endl;
+    cout<<"FLT_MIN: "<<(float)FLT_MIN<<endl;
+    cout<<"-FLT_MAX: "<<(float)-FLT_MAX<<endl;
+    cout<<"-FLT_MIN: "<<(float)-FLT_MIN<<endl;
+
+    cout<<"\nDBL_MAX: "<<(double)DBL_MAX<<endl;
+    cout<<"DBL_MIN: "<<(double)DBL_MIN<<endl;
+    cout<<"-DBL_MAX: "<<(double)-DBL_MAX<<endl;
+    cout<<"-DBL_MIN: "<<(double)-DBL_MIN<<endl;
+    
+    cout<<"\nLDBL_MAX: "<<(double)LDBL_MAX<<endl;
+    cout<<"LDBL_MIN: "<<(double)LDBL_MIN<<endl;
+    cout<<"-LDBL_MAX: "<<(double)-LDBL_MAX<<endl;
+    cout<<"-LDBL_MIN: "<<(double)-LDBL_MIN<<endl;
+    
+    cout<<"\n"<<FLT_DIG<<endl;
+    cout<<DBL_DIG<<endl;
+    cout<<LDBL_DIG<<endl;
     return 0;
 }
 /*
@@ -36,4 +55,23 @@ UCHAR_MAX: 255
 UINT_MAX: 4294967295
 ULONG_MAX: 18446744073709551615
 USHRT_MAX: 65535
+
+FLT_MAX: 3.40282e+38
+FLT_MIN: 1.17549e-38
+-FLT_MAX: -3.40282e+38
+-FLT_MIN: -1.17549e-38
+
+DBL_MAX: 1.79769e+308
+DBL_MIN: 2.22507e-308
+-DBL_MAX: -1.79769e+308
+-DBL_MIN: -2.22507e-308
+
+LDBL_MAX: inf
+LDBL_MIN: 0
+-LDBL_MAX: -inf
+-LDBL_MIN: -0
+
+6
+15
+18
 */
